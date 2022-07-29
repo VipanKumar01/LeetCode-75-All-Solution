79108896c5af24dc80fe0578579b43f961e14468
Vipan Kumar <108624219+VipanKumar01@users.noreply.github.com>
2022-07-29 20:34:28 +0530

    Update 205. Isomorphic Strings.py

--- 205. Isomorphic Strings.py
+++ 205. Isomorphic Strings.py
@@ -1,9 +1,18 @@
+# Name = Vipan Kumar
+# GitHub user name = @VipanKumar01
+
+
+
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         if len(s)!=len(t) or len(set(s))!=len(set(t)):
             return False
         hashmap={}
         
+# Name = Vipan Kumar
+# GitHub user name = @VipanKumar01
+
+
         for i in range(len(s)):
             if s[i] not in hashmap:
                 hashmap[s[i]]=t[i]
@@ -11,3 +20,6 @@ class Solution:
             if hashmap[s[i]]!=t[i]:
                 return False
         return True
+
+    
+ # -- Happy Code --    
