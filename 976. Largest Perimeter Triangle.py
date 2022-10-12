19c63ad712e6e1f9b54a2081f1481ed05cee8418
Vipan Kumar <108624219+VipanKumar01@users.noreply.github.com>
2022-10-12 08:28:32 -0700

    Create 976. Largest Perimeter Triangle.py

---
+++ 976. Largest Perimeter Triangle.py
@@ -0,0 +1,16 @@
+# Name = Vipan Kumar
+# GitHub user name = @VipanKumar01
+
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        
+        nums = sorted(nums)
+        
+        for i in range(len(nums)-1,1,-1):
+            if nums[i+2] + nums[i+1] > nums[i]:
+                return nums[i-2] + nums[i-1] + nums[i]
+            
+        return 0
+      
+      
+#  --HappyCode--
